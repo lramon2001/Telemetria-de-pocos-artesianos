16ebbfb7c3ad4d39655fefc48008a418cda8f41a
Felipe Alef <51218895+Alef012@users.noreply.github.com>
2022-06-09 17:24:59 -0300

    Create firebase.cpp

---
+++ firebase.cpp
@@ -0,0 +1,42 @@
+
+#include <WiFi.h>         
+#include <IOXhop_FirebaseESP32.h>                           
+#include <ArduinoJson.h>                   
+
+#define WIFI_SSID "nome do wifi"                   
+#define WIFI_PASSWORD "senha do wifi"         
+#define FIREBASE_HOST "link do firebase"    
+#define FIREBASE_AUTH "senha do banco"   
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
+  Serial.print("Conectando ao wifi");
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+}
+
+void loop() {
+
+//Exemplo da função Set
+
+
+  Firebase.setInt("/potenciometro/valor",analogRead(2));
+
+  delay(3000);
+
+
+
+}
